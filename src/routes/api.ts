d4aec24a6bb8370f5494ec509a23701fd607c8ee
Hasan <hasan.jafri7@gmail.com>
2023-08-30 00:12:01 -0400

    Added sample API router, added basicAuthMiddleware for bull-board

---
+++ src/routes/api.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+import Paths from '@src/constants/Paths';
+
+const apiRouter = Router();
+
+export default apiRouter;
---
+++ src/routes/middleware/basicAuth.ts
@@ -0,0 +1,24 @@
+import passport from 'passport';
+import { BasicStrategy } from 'passport-http';
+
+import EnvVars from '@src/constants/EnvVars';
+
+interface User {
+  name: string;
+}
+
+const basicAuthOptions: passport.AuthenticateOptions = {
+  session: false,
+};
+
+passport.use(
+  new BasicStrategy((username, password, done) => {
+    if (username === EnvVars.BullUser && password === EnvVars.BullPassword) {
+      return done(null, { name: 'admin' } as User);
+    } else {
+      return done(null, false);
+    }
+  }),
+);
+
+export const basicAuthMiddleware = passport.authenticate('basic', basicAuthOptions);
